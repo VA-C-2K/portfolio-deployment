8b2bbecdb1b35ef17aae4327757afced8bf9b8dd
Viraj-creation312 <virajjerry123@gmail.com>
2022-08-14 22:38:04 +0530

    fixing navigation dots and some data addon

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -4,7 +4,7 @@ const NavigationDots = ({ active }) => (
   <div className="app__navigation">
     {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
       <a
-        href={`#`}
+        href={`#${item}`}
         key={item + index}
         className="app__navigation-dot"
         style={active === item ? { backgroundColor: '#313BAC' } : {}}
